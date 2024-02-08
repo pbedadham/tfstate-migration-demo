4ef061c56ed1be647e5be001d0e61d92ed881f4d
Pavan Bedadham <pbedadham@gmail.com>
2024-02-07 19:52:47 -0800

    tf backend change

--- backend.tf
+++ backend.tf
@@ -3,8 +3,8 @@
 terraform {
   backend "s3" {
     bucket         = "demo-tf-state-s3" # your-remote-state-bucket-name
-    #key            = "terraform.tfstate" # Original Path
-    key            = "global/app1/terraform.tfstate" # New Path
+    key            = "terraform.tfstate" # Original Path
+    #key            = "global/app1/terraform.tfstate" # New Path
     region         = "us-east-2"  # Change this to your desired AWS region
     encrypt        = true
     dynamodb_table = "terraform-lock"  # Optional: Enables state locking with DynamoDB
